e3824c7176103b7b290757908976cb9fc41b5634
Roger Jungemann <roger@thefifthcircuit.com>
2012-11-21 09:15:54 -0800

    started adding more specs

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,11 +2,15 @@ require 'spec_helper'
 
 describe 'Welcome', :js => true do
 
-  it '' do
+  it 'shows a welcome page' do
     visit root_path
 
     page.should have_content('Txtur')
+
+    pending
   end
+
+  it 'shows a list of recent posts'
 
 end
 
