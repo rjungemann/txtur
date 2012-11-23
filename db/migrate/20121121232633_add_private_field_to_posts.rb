b7bdd52ce857c12338b8b90a12764048a8ae1a56
Roger Jungemann <roger@thefifthcircuit.com>
2012-11-22 22:17:42 -0800

    modified migrations because of discrepancy between sqlite and postgres

--- db/migrate/20121121232633_add_private_field_to_posts.rb
+++ db/migrate/20121121232633_add_private_field_to_posts.rb
@@ -1,7 +1,7 @@
 class AddPrivateFieldToPosts < ActiveRecord::Migration
 
   def change
-    add_column :posts, :private, :boolean, :default => 0
+    add_column :posts, :private, :boolean
   end
 
 end
--- db/migrate/20121122175616_change_private_to_public_for_posts.rb
+++ db/migrate/20121122175616_change_private_to_public_for_posts.rb
@@ -1,6 +1,6 @@
 class ChangePrivateToPublicForPosts < ActiveRecord::Migration
   def change
     remove_column :posts, :private, :boolean
-    add_column :posts, :public, :boolean, :default => 1
+    add_column :posts, :public, :boolean
   end
 end
